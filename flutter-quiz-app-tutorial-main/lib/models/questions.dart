e7133b353690ae9a133f6fca55176fad709fa3e7
wavewave1122 <645051200105@mail.rmutk.ac.th>
2024-02-13 15:11:58 +0700

    first commit

---
+++ flutter-quiz-app-tutorial-main/lib/models/questions.dart
@@ -0,0 +1,107 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  //คำถาม
+  Question(
+    question: '1.เมื่องหลวงของญี่ปุ่นคือ?',
+    correctAnswerIndex: 1,
+    options: [
+      'a)เกียวโต',
+      'b)โตเกียว',
+      'c)โอซาก้า',
+      'd)ฮอกไกโด',
+    ],
+  ),
+  Question(
+    question: '2.ญี่ปุ่นตั้งอยู่ในทวีปใด?',
+    correctAnswerIndex: 3,
+    options: [
+      'a)ทวีปออสเตรเลีย',
+      'b)ทวีปอเมริกาใต้',
+      'c)ทวีปยุโรป',
+      'd)เอเชีย',
+    ],
+  ),
+  Question(
+    question: '3.เกาะที่ใหญ่ที่สุดในญี่ปุ่นคือที่ใด?',
+    correctAnswerIndex: 1,
+    options: [
+      'a)ชิค็อกุ',
+      'b)ชิค็อกุ',
+      'c)ชิค็อกุ',
+      'd)โอกินาว่า',
+    ],
+  ),
+  Question(
+    question: '4.สัตว์ประจำชาติของญี่ปุ่นคือ?',
+    correctAnswerIndex: 0,
+    options: [
+      'a)นกกระเรียน',
+      'b)ปลาคาร์ฟ',
+      'c)แมว',
+      'd)อีกา',
+    ],
+  ),
+  Question(
+    question: '5.ภูเขาสูงสุดในญี่ปุ่นคืออะไร?',
+    correctAnswerIndex: 2,
+    options: [
+      'a)คิริชิมะ',
+      'b)ซุกูระจิมา',
+      'c)ฟูจิ',
+      'd)อันนาปุระ',
+    ],
+  ),
+  Question(
+    question: '6.ญี่ปุ่นมีกี่จังหวัด?',
+    correctAnswerIndex: 0,
+    options: [
+      'a)47',
+      'b)45',
+      'c)49',
+      'd)43',
+    ],
+  ),
+  Question(
+    question: '7.ซากุระจะบานในช่วงเดือนอะไร?',
+    correctAnswerIndex: 0,
+    options: [
+      'a)เมษา-พฤษภา',
+      'b)พฤศจิกา-ธันวา',
+      'c)กรกฎา-สิงหา',
+      'd)มกรา-กุมภา',
+    ],
+  ),
+  Question(
+    question: '8.อาหารขึ้นชื่อของญี่ปุ่นคือ?',
+    correctAnswerIndex: 1,
+    options: [
+      'a)ซาชิมิ',
+      'b)ซูชิ',
+      'c)ราเม็ง',
+      'd)เทมปุระ',
+    ],
+  ),
+  Question(
+    question:
+        '9.วัดที่ดังที่สุดในญี่ปุ่นคือที่ใด?',
+    correctAnswerIndex: 3,
+    options: [
+      'a)โตโดจิ',
+      'b)โคโยทิ',
+      'c)ฟูจิซาวะ',
+      'd)เซ็นโซจิ',
+    ],
+  ),
+  Question(
+    question:
+        '10.ญี่ปุ่นจังหวัดอะไรมีประชากรมากที่สุด?',
+    correctAnswerIndex: 2,
+    options: [
+      'a)โอซาก้า',
+      'b)เกียวโต',
+      'c)โตเกียว',
+      'd)ฮอกไกโด',
+    ],
+  ),
+];
